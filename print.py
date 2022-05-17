be65c6e2cc919e97e117957a9a63feaa6fac396c
yooong <dldydgnl2080@dgu.ac.kr>
2022-05-17 15:29:39 +0900

    Add univ

--- print.py
+++ print.py
@@ -1,8 +1,10 @@
 name=input('이름을 입력하세요: ')
 code=input('학번을 입력하세요: ')
+dep=input('학과를 입력하세요: ')
 univ=input('대학교를 입력하세요: ')
 
 print('')
 print('이름:', name)
 print('학번:', code)
+print('학과:', dep)
 print('대학교:', univ)
